784b6f092bfc599685ff8716d1b47fd807d908db
Christinamawel <christinamawel@gmail.com>
2022-06-27 12:08:08 -0700

    creating simple GUI

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,16 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("what is your name?");
-        String name = scanner.nextLine();
-        System.out.println("how old are you?");
-        int age = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println("whats your favorite food?");
-        String food = scanner.nextLine();
+        String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null, "Hello " + name);
 
-        System.out.println("Hello " + name);
-        System.out.println("You are " + age + " years old");
-        System.out.println("you like to eat " + food);
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null, "you are " + age + " years old");
 
+        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height (in ft)"));
+        JOptionPane.showMessageDialog(null, "you are " + height + " feet tall");
     }
 }
