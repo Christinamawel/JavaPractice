1b63e0be3da2d04ca832a41792f88a78c3eaa21d
Christinamawel <christinamawel@gmail.com>
2022-06-30 15:35:55 -0700

    add functionality up to 9th frame for bowling calc

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import static java.lang.Integer.parseInt;
 
 public class Main {
 
-    static int turn = 1;
+    static int turn = 0;
     static int throwNum = 0;
     static int score = 0;
     static String[][] scoreboard = {
@@ -15,7 +15,8 @@ public class Main {
             {"-","-"},
             {"-","-"},
             {"-","-"},
-            {"-","-"}
+            {"-","-"},
+            {"-","-","-"}
     };
 
     public static void main(String[] args) {
@@ -30,54 +31,61 @@ public class Main {
 
     public static void bowl() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("-----------Bowling Score Calculator-----------");
-        for(int i = 0; i < scoreboard.length; i++) {
-            System.out.println("Frame " + (i+1) + ": [" + scoreboard[i][0] + "] [" + scoreboard[i][1] + "]");
+        while(turn <= 8) {
+            System.out.println("-----------Bowling Score Calculator-----------");
+            for(int i = 0; i < scoreboard.length; i++) {
+                System.out.println("Frame " + (i+1) + ": [" + scoreboard[i][0] + "] [" + scoreboard[i][1] + "]");
+            }
+            System.out.println("current total score: " + score);
+            System.out.println("please type your scores for each throw");
+            System.out.println("type 10 for strikes and spares (even if less pins than 10 were hit for spare)");
+            System.out.println("Score this throw: ");
+            System.out.println("turn: " + turn + " || throwNum: " + throwNum);
+            int currentThrow = scanner.nextInt();
+            Main.scoreCalc(currentThrow);
         }
-        System.out.println("current total score: " + score);
-        System.out.println("please type your scores for each throw");
-        System.out.println("type 10 for strikes and spares (even if less pins than 10 were hit for spare)");
-        System.out.println("Score this throw: ");
-        int currentThrow = scanner.nextInt();
-        Main.scoreCalc(currentThrow);
     }
 
     public static void scoreCalc(int currentThrow) {
         Scanner scanner = new Scanner(System.in);
-        if (currentThrow == 10 && throwNum == 0) {
-            scoreboard[turn][0] = "-";
-            scoreboard[turn][1] = "X";
-            if(scoreboard[turn - 1][1] == "X" && scoreboard[turn - 2][1] == "X") {
-                score += 30;
-            }
-            throwNum ++;
-            turn++;
-        } else if (currentThrow == 10 && throwNum == 1) {
-            scoreboard[turn][1] = "/";
-            if(scoreboard[turn - 1][1] == "X" && scoreboard[turn - 2][1] == "X") {
-                score += 30;
-            }
-            throwNum --;
-        } else if (currentThrow < 10 && currentThrow >= 0) {
-            if(throwNum == 0) {
-                if (scoreboard[turn - 1][1] == "/") {
-                    score += 10 + currentThrow;
-                }
-                scoreboard[turn][0] = String.valueOf(currentThrow);
-                throwNum ++;
-            } else {
-                int frameScore = currentThrow + parseInt(scoreboard[turn][0]);
-                if(scoreboard[turn - 1][1] == "X") {
-                    if(scoreboard[turn - 2][1] == "X") {
-                        score += 30 + (frameScore * 2);
-                    } else {
-                        score += 10 + (frameScore * 2);
-                    }
-                } else {
-                    score += frameScore;
-                }
-                scoreboard[turn][1] = String.valueOf(currentThrow);
+        if (currentThrow == 10) {
+            if (throwNum == 1) {
+                scoreboard[turn][1] = "/";
                 turn++;
+                throwNum = 0;
+            } else if (throwNum == 0) {
+                scoreboard[turn][1] = "X";
+                if (turn >= 2) {
+                    if (scoreboard[turn - 1][1] == "X" && scoreboard[turn - 2][1] == "X") {
+                        score += 30;
+                    }
+                }
+                turn++;
+            }
+        } else if (currentThrow < 10 && currentThrow >= 0) {
+            scoreboard[turn][throwNum] = String.valueOf(currentThrow);
+            if (throwNum == 0) {
+                if (turn > 0) {
+                    if (scoreboard[turn - 1][1] == "/") {
+                        score += 10 + currentThrow;
+                    }
+                }
+                throwNum = 1;
+            } else if (throwNum == 1) {
+                int frameTotal = currentThrow + Integer.parseInt(scoreboard[turn][0]);
+                score += frameTotal;
+                if (turn > 0) {
+                    if (scoreboard[turn - 1][1] == "X") {
+                        score += 10 + frameTotal;
+                        if (turn >= 2) {
+                            if (scoreboard[turn - 2][1] == "X") {
+                                score += 20 + frameTotal;
+                            }
+                        }
+                    }
+                }
+                turn++;
+                throwNum = 0;
             }
         } else {
             System.out.println("---------not a valid score---------");
